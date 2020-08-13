0f4aa8a676df5477bb45a41d9252a5466045533a
Marvin9 <marvinduff97@gmail.com>
2020-08-13 21:34:38 +0530

    chore: Use env port if defined

--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"os"
+
 	"github.com/Marvin9/ignitus-mailing-microservice/api"
 	"github.com/gin-gonic/gin"
 	_ "github.com/joho/godotenv/autoload"
 )
+
+func usePort() string {
+	port := os.Getenv("PORT")
+	if port != "" {
+		return ":" + port
+	}
+	return ":8000"
+}
 
 func main() {
 	router := gin.Default()
 
 	router.POST("/mail/confirmation", api.ConfirmationMailAPI)
 
-	router.Run(":8000")
+	router.Run(usePort())
 }
